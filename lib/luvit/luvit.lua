147edccb2cd3a4dc50ec2a5d9a263d6918fe9c97
Brandon Philips <brandon.philips@rackspace.com>
2012-02-09 18:10:03 -0800

    luvit.lua: don't use tty on windows
    Windows's TTYs aren't pipes. Copy the logic from nodejs
    https://github.com/joyent/node/blob/master/src/node.js#L221-274

--- lib/luvit/luvit.lua
+++ lib/luvit/luvit.lua
@@ -40,6 +40,7 @@ local table = require('table')
 local utils = require('utils')
 local fs = require('fs')
 local Tty = require('tty').Tty
+local Pipe = require('pipe').Pipe
 local Emitter = require('core').Emitter
 local constants = require('constants')
 local path = require('path')
@@ -118,14 +119,48 @@ if OS_BINDING.type() ~= "win32" then
   uv.unref()
 end
 
+local createWriteableStdioStream = function(fd)
+  local fd_type = uv.handleType(fd);
+  if (fd_type == "TTY") then
+    local tty = Tty:new(fd)
+    uv.unref()
+    return tty
+  elseif (fd_type == "FILE") then
+    return fs.SyncWriteStream:new(fd)
+  elseif (fd_type == "NAMED_PIPE") then
+    local pipe = Pipe:new(nil)
+    pipe:open(fd)
+    uv.unref()
+    return pipe
+  else
+    error("Unknown stream file type " .. fd)
+  end
+end
+
+local createReadableStdioStream = function(fd)
+  local fd_type = uv.handleType(fd);
+  if (fd_type == "TTY") then
+    local tty = Tty:new(fd)
+    uv.unref()
+    return tty
+  elseif (fd_type == "FILE") then
+    return fs.createReadStream(nil, {fd = fd})
+  elseif (fd_type == "NAMED_PIPE") then
+    local pipe = Pipe:new(nil)
+    pipe:open(fd)
+    uv.unref()
+    return pipe
+  else
+    error("Unknown stream file type " .. fd)
+  end
+end
+
 -- Load the tty as a pair of pipes
 -- But don't hold the event loop open for them
-process.stdin = Tty:new(0)
-process.stdout = Tty:new(1)
+process.stdin = createReadableStdioStream(0)
+process.stdout = createWriteableStdioStream(1)
+process.stderr = createWriteableStdioStream(2)
 local stdout = process.stdout
-uv.unref()
-uv.unref()
-
 
 -- Replace print
 function print(...)
