3d9b2e96473887bff056c09f552a28746595f4c6
Brandon Philips <brandon.philips@rackspace.com>
2012-03-23 16:23:18 -0700

    tests: tls: test-tls-client-abort enable test
    WIP: figure out why the socket doesn't get closed and holds the event loop
    open. Hrm.

--- tests/tls/test-tls-client-abort.lua
+++ tests/tls/test-tls-client-abort.lua
@@ -1,21 +1,20 @@
 require('helper')
-local fixture = require('./fixture-tls')
+local path = require('path')
+local fixture = require(path.join(__dirname, 'fixture-tls'))
+local childprocess = require('childprocess')
 local tls = require('tls')
 
--- local options = {
---   cert = fixture.certPem,
---   key = fixture.certKey,
---   port = fixture.commonPort
--- }
---
--- local conn = tls.connect(options, function()
---   assert(true)
--- end)
---
--- conn:on('error', function()
--- end)
---
--- doesnt_throw(function()
---   conn:close()
--- end)
---
+local options = {
+  cert = fixture.certPem,
+  key = fixture.keyPem,
+  port = 5121,
+  host = '0.0.0.0'
+}
+
+local conn = tls.connect(options, function()
+  assert(false); -- callback should never be executed
+end)
+conn:on('error', function()
+  print("error")
+  conn:destroy();
+end)
